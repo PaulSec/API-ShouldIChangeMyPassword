56d415eb0cad63baaeb43942bccaac5a349fbbbd
Paul AMAR <aos.paul@gmail.com>
2014-06-01 14:30:26 +0200

    Deleted old files after renaming

--- sicmpAPI_example.py
+++
@@ -1,5 +0,0 @@
-from sicmpAPI import SicmpAPI
-res = SicmpAPI().is_compromised('myprivatemail@mypersonaldomain.com')
-print res  # False
-res = SicmpAPI().is_compromised('test@test.com')
-print res  # True
